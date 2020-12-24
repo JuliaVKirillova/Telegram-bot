1c4b9c0c4402059d95c59f6e1c6fa2817aba653a
Julia <vay-voo@mail.ru>
2020-12-24 19:19:55 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+API Telegram Bot
+
+Personal telegram bot for checking the user's status on the social network vk.com. 
+Interaction with the API Telegram and API vk.com. 
+Procfile for Heroku deployment.
---
+++ my_bot.py
@@ -0,0 +1,46 @@
+import os
+import telegram
+import time
+import requests
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+URL = os.getenv('URL')
+V = os.getenv('VK_API_VERSION')  
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+
+
+def get_user_status(user_id):
+    params = {
+        'user_ids': user_id,
+        'fields': 'online',
+        'v': V,
+        'access_token': ACCESS_TOKEN
+    }
+    try:
+        user_status = requests.post(URL, params=params).json()['response'][0]['online']
+        user_name = requests.post(URL, params=params).json()['response'][0]['first_name']
+        return user_status, user_name
+    except requests.exceptions.RequestException as e:
+        logging.exception(f'Request raised error: {e}')  
+
+
+
+def send_message(message):
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
+
+
+
+if __name__ == "__main__":
+    vk_id = input("Введите id друга ")
+    while True:
+        status, name = get_user_status(vk_id)
+        if status == 1:
+            send_message(f'{name} сейчас онлайн!')
+            break
+        time.sleep(1200)
